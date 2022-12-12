270ffdd7768aef3d71147dc654474bed2db42e35
andreashunziker@gmx.net <andreashunziker@gmx.net>
2022-12-12 11:34:11 +0100

    improved error messages

--- 2/2.c
+++ 2/2.c
@@ -8,7 +8,7 @@ static int shape_score(char shape){
     case 'X': return 1; break; // Rock
     case 'Y': return 2; break; // Paper
     case 'Z': return 3; break; // Sciccor
-    default: printf("unknown symbol for shape"); return -1; break;
+    default: printf("unknown symbol for shape: %c\n",shape); return -1; break;
     }
 }
 
@@ -19,23 +19,23 @@ static int outcome_score(char opponentChoice, char myChoice){
         case 'X': return 3; break; // Rock -> Draw
         case 'Y': return 6; break; // Paper -> Win
         case 'Z': return 0; break; // Sciccor -> Lose
-        default: printf("unknown symbol for my choice"); return -1; break;
+        default: printf("unknown symbol for my choice: %c\n",myChoice); return -1; break;
         }
     case 'B': // Paper
         switch (myChoice){
         case 'X': return 0; break; // Rock -> Lose
         case 'Y': return 3; break; // Paper -> Draw
         case 'Z': return 6; break; // Sciccor -> Win
-        default: printf("unknown symbol for my choice"); return -1; break;
+        default: printf("unknown symbol for my choice: %c\n",myChoice); return -1; break;
         }
     case 'C': // Sciccor
         switch (myChoice){
         case 'X': return 6; break; // Rock -> Win
         case 'Y': return 0; break; // Paper -> Lose
         case 'Z': return 3; break; // Sciccor -> Draw
-        default: printf("unknown symbol for my choice"); return -1; break;
+        default: printf("unknown symbol for my choice: %c\n",myChoice); return -1; break;
         }
-    default: printf("unknown symbol for opponent choice"); return -1;
+    default: printf("unknown symbol for opponent choice: %c\n",opponentChoice); return -1;
     }
 }
 
