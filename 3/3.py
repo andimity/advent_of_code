5d85b93ec71bd9253c86b648b23fca3d7656dfe2
andreashunziker@gmx.net <andreashunziker@gmx.net>
2022-12-12 21:00:04 +0100

    split itemList

--- 3/3.py
+++ 3/3.py
@@ -1,1 +1,9 @@
-print("hello world!")
+with open("3/input.txt") as f:
+    # itemList = f.readline()
+    itemList = "vJrwpWtwJgWrhcsFMMfFFhFp"
+    itemListHalf = int(len(itemList) / 2)
+    compartment1 = itemList[0:itemListHalf]
+    compartment2 = itemList[itemListHalf:-1]
+
+    print(f"itemList: {itemList} \nfirst half: {compartment1} \nsecond half: {compartment2}")
+    
