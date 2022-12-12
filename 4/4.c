d2cf0e465429b597d330a4cdb18b4b3f31e0caf7
andreashunziker@gmx.net <andreashunziker@gmx.net>
2022-12-12 22:45:33 +0100

    init commit

---
+++ 4/4.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[])
+{   
+    char s[] = "712";
+    int i = atoi(s) / 4;
+    printf("hello world!\n");
+    printf("i has the value %d\n",i);
+    return 0;
+}
