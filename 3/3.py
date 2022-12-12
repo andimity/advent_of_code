81f552eec4234e5dfcfb8bda95c24e8fc1736e1e
andreashunziker@gmx.net <andreashunziker@gmx.net>
2022-12-12 21:33:22 +0100

    solution part 1

--- 3/3.py
+++ 3/3.py
@@ -2,13 +2,25 @@ def find_common_item(itemList1:str, itemList2:str)->str:
     return list(set(itemList1).intersection(set(itemList2)))[0]
 
 
-with open("3/input.txt") as f:
-    # itemList = f.readline()
-    itemList = "vJrwpWtwJgWrhcsFMMfFFhFp"
-    itemListHalf = int(len(itemList) / 2)
-    compartment1 = itemList[0:itemListHalf]
-    compartment2 = itemList[itemListHalf:]
+def calculate_item_priority(item:str)->int:
+    if ord('a')<=ord(item):
+        return 1 + ord(item)-ord('a')
+    else:
+        return 27 + ord(item)-ord('A')
 
-    print(f"itemList: {itemList} \nfirst half: {compartment1} \nsecond half: {compartment2}")
-    print(f"common item: {find_common_item(compartment1,compartment2)}")
+
+if __name__ == "__main__":
     
+    prioritySum = 0
+
+    with open("3/input.txt") as inputfile:
+        for itemList in inputfile:
+            itemListHalf = int(len(itemList) / 2)
+            compartment1 = itemList[0:itemListHalf]
+            compartment2 = itemList[itemListHalf:]
+
+            commonItem = find_common_item(compartment1,compartment2)
+            prioritySum += calculate_item_priority(commonItem)
+    
+    print(f"solution to part1: {prioritySum}")
+
