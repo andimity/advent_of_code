1465d3c01ac6a6ef066554fbf6a0e978dfc9523a
andreashunziker@gmx.net <andreashunziker@gmx.net>
2022-12-12 11:57:25 +0100

    completed part 2

--- 2/2.c
+++ 2/2.c
@@ -12,7 +12,7 @@ static int shape_score(char shape){
     }
 }
 
-static int outcome_score(char opponentChoice, char myChoice){
+static int outcome_score_1(char opponentChoice, char myChoice){
     switch (opponentChoice){
     case 'A': // Rock
         switch (myChoice){
@@ -40,22 +40,55 @@ static int outcome_score(char opponentChoice, char myChoice){
 }
 
 
-static inline int calculate_score(char opponentChoice, char myChoice){
-    return outcome_score(opponentChoice, myChoice) + shape_score(myChoice);
+static inline int calculate_score_1(char opponentChoice, char myChoice){
+    return outcome_score_1(opponentChoice, myChoice) + shape_score(myChoice);
+}
+
+
+static int calculate_score_2(char opponentChoice, char myChoice){
+    switch (opponentChoice){
+    case 'A': // Rock
+        switch (myChoice){
+        case 'X': return 0+3; break; // Lose -> Sciccor
+        case 'Y': return 3+1; break; // Draw -> Rock
+        case 'Z': return 6+2; break; // Win -> Paper
+        default: printf("unknown symbol for my choice: %c\n",myChoice); return -1; break;
+        }
+    case 'B': // Paper
+        switch (myChoice){
+        case 'X': return 0+1; break; // Lose -> Rock
+        case 'Y': return 3+2; break; // Draw -> Paper
+        case 'Z': return 6+3; break; // Win -> Sciccor
+        default: printf("unknown symbol for my choice: %c\n",myChoice); return -1; break;
+        }
+    case 'C': // Sciccor
+        switch (myChoice){
+        case 'X': return 0+2; break; // Lose -> Paper
+        case 'Y': return 3+3; break; // Draw -> Sciccor
+        case 'Z': return 6+1; break; // Win -> Rock
+        default: printf("unknown symbol for my choice: %c\n",myChoice); return -1; break;
+        }
+    default: printf("unknown symbol for opponent choice: %c\n",opponentChoice); return -1;
+    }
 }
 
 
 int main(int argc, char const *argv[])
 {   
-    int score = 0;
-    char i[5];
+    int score1 = 0;
+    int score2 = 0;
+    char opponentChoice;
+    char myChoice;
+    char buffer[5];
     FILE* input = fopen("input.txt","r");
-    while(fgets(i,sizeof(i),input) != NULL){        
-        // printf("opponentChoice: %c, myChoice: %c\n",i[0],i[2]);
-        // printf("score: %d\n", calculate_score(i[0],i[2]));
-        score += calculate_score(i[0], i[2]);
+    while(fgets(buffer,sizeof(buffer),input) != NULL){   
+        opponentChoice = buffer[0];
+        myChoice = buffer[2];
+        score1 += calculate_score_1(opponentChoice, myChoice);
+        score2 += calculate_score_2(opponentChoice, myChoice);
     }
     fclose(input);
-    printf("total score: %d\n",score);
+    printf("total score part1: %d\n",score1);
+    printf("total score part2: %d\n",score2);
     return 0;
 }
