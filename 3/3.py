c2fbaf06634d864d1b971fb4cee644b08c488664
andreashunziker@gmx.net <andreashunziker@gmx.net>
2022-12-12 21:11:26 +0100

    extract common item

--- 3/3.py
+++ 3/3.py
@@ -1,9 +1,14 @@
+def find_common_item(itemList1:str, itemList2:str)->str:
+    return list(set(itemList1).intersection(set(itemList2)))[0]
+
+
 with open("3/input.txt") as f:
     # itemList = f.readline()
     itemList = "vJrwpWtwJgWrhcsFMMfFFhFp"
     itemListHalf = int(len(itemList) / 2)
     compartment1 = itemList[0:itemListHalf]
-    compartment2 = itemList[itemListHalf:-1]
+    compartment2 = itemList[itemListHalf:]
 
     print(f"itemList: {itemList} \nfirst half: {compartment1} \nsecond half: {compartment2}")
+    print(f"common item: {find_common_item(compartment1,compartment2)}")
     
