f59291f8e1c3b40a599f089468e2bc21c756b3dc
andreashunziker@gmx.net <andreashunziker@gmx.net>
2022-12-13 20:25:37 +0100

    read whole file

--- 4/4.c
+++ 4/4.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
 #define BUFFERSIZE      5
 
 
@@ -18,13 +19,21 @@ int get_int_from_file(FILE* inputfile){
 int main(int argc, char const *argv[])
 {   
     FILE* inputfile = fopen("input.txt","r");
-    int start1 = get_int_from_file(inputfile);
-    int end1 = get_int_from_file(inputfile);
-    int start2 = get_int_from_file(inputfile);
-    int end2 = get_int_from_file(inputfile);
+    int start1,start2,end1,end2;
+
+    do{
+    start1 = get_int_from_file(inputfile);
+    end1 = get_int_from_file(inputfile);
+    start2 = get_int_from_file(inputfile);
+    end2 = get_int_from_file(inputfile);
+
+    if(!start1)break;
 
     printf("range 1: %d - %d\n",start1,end1);
     printf("range 2: %d - %d\n",start2,end2);
+    } while (true);
+    
+
 
     fclose(inputfile);
     return 0;
