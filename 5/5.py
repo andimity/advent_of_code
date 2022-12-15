c5e0e8143cea75c87a0ff29234bd5c0d6ad9363b
andreashunziker@gmx.net <andreashunziker@gmx.net>
2022-12-15 01:46:30 +0100

    solution to part 2

--- 5/5.py
+++ 5/5.py
@@ -1,4 +1,5 @@
 import re
+import copy
 
 FILENAME = "5/input.txt"
 
@@ -16,16 +17,22 @@ INITIAL_STACKS = [
 ]
 
 
-
-def execute_inctruction(stacks:list, quantity:int, source:int, target:int):
+def move_single_crates(stacks:list, quantity:int, source:int, target:int):
     for _ in range(quantity):
-        cargo = stacks[source].pop()
-        stacks[target].append(cargo)
+        crate = stacks[source].pop()
+        stacks[target].append(crate)
+
+
+def move_multiple_crates(stacks:list, quantity:int, source:int, target:int):
+    crates:list = stacks[source][-quantity:]
+    for _ in range(quantity):
+        stacks[source].pop()
+    stacks[target].extend(crates)
 
 
 if __name__ == "__main__":
-
-    stacks = INITIAL_STACKS
+    stacksPart1 = copy.deepcopy(INITIAL_STACKS)
+    stacksPart2 = copy.deepcopy(INITIAL_STACKS)
 
     with open(FILENAME, 'r') as f:
         instructions = f.readlines()
@@ -36,7 +43,10 @@ if __name__ == "__main__":
         source = int(variables.group(2)) - 1
         target = int(variables.group(3)) - 1
 
-        execute_inctruction(stacks,quantity,source,target)
+        move_single_crates(stacksPart1,quantity,source,target)
+        move_multiple_crates(stacksPart2,quantity,source,target)
     
-    result = "".join(stack[-1] for stack in stacks)
-    print(result)
+    result1 = "".join(stack[-1] for stack in stacksPart1)
+    result2 = "".join(stack[-1] for stack in stacksPart2)
+    print(f"solution to part 1: {result1}")
+    print(f"solution to part 2: {result2}")
