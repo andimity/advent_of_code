1aaf982941bd973ca9bd0ad05b3fc190e587d581
andreashunziker@gmx.net <andreashunziker@gmx.net>
2022-12-19 23:16:19 +0100

    sum of unique directorynames

--- 7/7.py
+++ 7/7.py
@@ -34,6 +34,18 @@ def directory_size(directoryName:str)->int:
     return directorySize
 
 
+def sum_directory_sizes_smaller_than(limit:int)->int:
+    global directories
+    sum = 0
+    for directory in directories:
+        size = directory_size(directory)
+        if size<=limit:
+            sum += size
+    
+    return sum
+
+
+
 if __name__ == "__main__":
     
     currentPath:str = ''
@@ -59,5 +71,10 @@ if __name__ == "__main__":
         newItem = item(name=itemName,size=int(itemSize),parents=itemParents)
         filesystem.append(newItem)
 
+    print(filesystem)
+
     for directory in directories:
         print(f"{directory}:{directory_size(directory)}")
+
+    print(f"solution to part 1: {sum_directory_sizes_smaller_than(100000)}")
+    # 1072909 too small
