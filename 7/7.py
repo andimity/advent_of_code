60772632c6348e16fbc726b3d61189966c5366ff
andreashunziker@gmx.net <andreashunziker@gmx.net>
2022-12-19 23:44:43 +0100

    solution to part 2

--- 7/7.py
+++ 7/7.py
@@ -45,8 +45,23 @@ def sum_directory_sizes_smaller_than(limit:int)->int:
     return sum
 
 
+def smallest_directory_larger_than(limit:int)->int:
+    global directories
+    smallestDirectorySize = None
+
+    for directory in directories:
+        size = directory_size(directory)
+        if size>=limit:
+            if (smallestDirectorySize==None or size<smallestDirectorySize):
+                smallestDirectorySize = size
+    
+    return smallestDirectorySize
+
 
 if __name__ == "__main__":
+
+    DISC_SIZE = 70000000
+    UPDATE_SIZE = 30000000
     
     currentPath:str = ''
     filesystem:list[item] = []
@@ -68,5 +83,8 @@ if __name__ == "__main__":
         itemSize,itemName =  re.search(f"(.+) (.+)", line).groups()
         newItem = item(name=itemName,size=int(itemSize),parents=currentPath)
         filesystem.append(newItem)
-
+    
     print(f"solution to part 1: {sum_directory_sizes_smaller_than(100000)}")
+
+    missingSpace = UPDATE_SIZE - (DISC_SIZE - directory_size('/'))
+    print(f"solution to part 2: {smallest_directory_larger_than(missingSpace)}")
