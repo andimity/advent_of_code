4a8dc1f4311a4eb96cfe1250a8b0a14e975d41df
andreashunziker@gmx.net <andreashunziker@gmx.net>
2022-12-21 00:47:29 +0100

    read input into matrix

---
+++ 8/8.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define FILENAME        "test.txt"
+
+
+static int count_rows(const char *filename)
+{
+    FILE *file = fopen(filename,"r");
+    char c;
+    int line_counter = 0;
+    while(true){
+        c = getc(file);
+        if(c==EOF){
+            line_counter++;
+            break;
+        }
+        else if(c=='\n') line_counter++;
+    }
+    fclose(file);
+    return line_counter;
+}
+
+
+static int count_columns(const char *filename)
+{
+    FILE *file = fopen(filename,"r");
+    size_t len;
+    fgetln(file,&len);
+    fclose(file);
+    return (int)len-1;
+}
+
+static uint8_t** matrix_allocate(uint16_t rows, uint16_t cols)
+{   
+    uint8_t **matrix = (uint8_t**)malloc(sizeof(uint8_t*) * rows*cols*1);
+    for(uint16_t row=0; row<rows; row++){
+        matrix[row] = (uint8_t*)malloc(cols);
+    }
+
+    printf("%d x %d matrix allocated\n", rows, cols);
+    return matrix;
+}
+
+
+static void matrix_fill(uint8_t** matrix, uint16_t rows, uint16_t cols, const char* filename)
+{
+    FILE *file = fopen(filename,"r");
+    size_t len;
+    char* line;
+    for(uint16_t row=0; row<rows; row++){
+        line = fgetln(file,&len);
+        for(uint16_t col=0; col<cols; col++){
+            matrix[row][col] = (uint8_t)line[col] - '0';
+            // printf("matrix (%d,%d) = %d\n",row,col,matrix[row][col]);
+        }
+    }
+    printf("%d x %d matrix filled\n", rows, cols);
+    fclose(file);
+}
+
+
+int main(int argc, char const *argv[])
+{   
+    uint16_t rows = count_rows(FILENAME);
+    uint16_t cols = count_columns(FILENAME);
+    uint8_t **forest = matrix_allocate(rows,cols);
+
+    matrix_fill(forest,rows,cols,FILENAME);
+
+    free(forest);
+
+    return 0;
+}
