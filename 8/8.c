63772520751152000ad042fede46da6b10696815
andreashunziker@gmx.net <andreashunziker@gmx.net>
2022-12-23 12:02:51 +0100

    matrix operations in seperate file

--- 8/8.c
+++ 8/8.c
@@ -1,83 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
-#define FILENAME        "test.txt"
+#include "matrix.h"
+#include "8.h"
 
 
-static int count_rows(const char *filename)
-{
-    FILE *file = fopen(filename,"r");
-    char c;
-    int line_counter = 0;
-    while(true){
-        c = getc(file);
-        if(c==EOF){
-            line_counter++;
-            break;
-        }
-        else if(c=='\n') line_counter++;
-    }
-    fclose(file);
-    return line_counter;
-}
+bool tree_is_visible(Matrix *forest, Tree tree);
 
-
-static int count_columns(const char *filename)
-{
-    FILE *file = fopen(filename,"r");
-    size_t len;
-    fgetln(file,&len);
-    fclose(file);
-    return (int)len-1;
-}
-
-static uint8_t** matrix_allocate(uint16_t rows, uint16_t cols)
-{   
-    uint8_t **matrix = (uint8_t**) malloc(sizeof(uint8_t*) * rows * cols);
-    for(uint16_t row=0; row<rows; row++){
-        matrix[row] = (uint8_t*)malloc(cols);
-    }
-
-    printf("%d x %d matrix allocated\n", rows, cols);
-    return matrix;
-}
-
-
-static void matrix_free(uint8_t** matrix, uint16_t rows)
-{   
-    for(uint16_t row=0; row<rows; row++){
-        free((void*) matrix[row]);
-    }
-    free((void*) matrix);
-}
-
-
-static void matrix_fill(uint8_t** matrix, uint16_t rows, uint16_t cols, const char* filename)
-{
-    FILE *file = fopen(filename,"r");
-    size_t len;
-    char* line;
-    for(uint16_t row=0; row<rows; row++){
-        line = fgetln(file,&len);
-        for(uint16_t col=0; col<cols; col++){
-            matrix[row][col] = (uint8_t)line[col] - '0';
-        }
-    }
-    printf("%d x %d matrix filled\n", rows, cols);
-    fclose(file);
-}
 
 
 int main(int argc, char const *argv[])
 {   
-    uint16_t rows = count_rows(FILENAME);
-    uint16_t cols = count_columns(FILENAME);
-    uint8_t **forest = matrix_allocate(rows,cols);
+    // uint16_t rows = count_rows(FILENAME);
+    // uint16_t cols = count_columns(FILENAME);
+    // uint8_t **forest = matrix_allocate(rows,cols);
 
-    matrix_fill(forest,rows,cols,FILENAME);
+    // matrix_fill(forest,rows,cols,FILENAME);
 
-    matrix_free(forest,rows);
+    // matrix_free(forest,rows);
+    Matrix forest = new_matrix_from_file(FILENAME);
+    matrix_free(&forest);
 
     return 0;
 }
